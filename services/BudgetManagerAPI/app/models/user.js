dc4ca9215abca54445a29695016d54dfa6f4482e
Darren <darrenhay1994@gmail.com>
2017-11-25 13:16:05 +0200

    Creating User model

---
+++ services/BudgetManagerAPI/app/models/user.js
@@ -0,0 +1,45 @@
+const mongoose = require('mongoose'),
+  bcrypt = require('bcrpyt');
+
+const Schema = mongoose.Schema({
+  username: {
+    type: String,
+    unique: true,
+    required: true
+  },
+
+  password: {
+    type: String,
+    required: true
+  },
+
+  clients: [{}]
+});
+
+Schema.pre('save', function(next) {
+  const user = this;
+
+  if (this.isModified('password') || this.isNew) {
+    bcrypt.genSalt(10, (error, salt) => {
+      if (error) return next(error);
+
+      bcrypt.hash(user.password, salt, (error, hash) => {
+        if (error) return next(error);
+
+        user.password = hash;
+        next();
+      });
+    });
+  } else {
+    return next();
+  }
+});
+
+Schema.methods.comparePasswords = function(password, callback) {
+  bcrypt.compare(password, this.password, (error, matches) => {
+    if (error) return callback(error);
+    callback(null, matches);
+  });
+};
+
+mongoose.model('User', Schema);
---
+++ services/BudgetManagerAPI/config/index.js
@@ -0,0 +1,5 @@
+modules.exports = {
+  secret: 'budgetsecret',
+  session: { session: false},
+  database: 'mongodb://127.0.0.1:27017/budgetmanager'
+}
