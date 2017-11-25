1eacf56b081979bc516c580ac679bc5b932bdbf0
Darren <darrenhay1994@gmail.com>
2017-11-25 16:13:30 +0200

    Loading user model on setup

---
+++ services/BudgetManagerAPI/app/api/auth.js
@@ -0,0 +1,3 @@
+const mongoose = require('mongoose'),
+  jwt = require('jsonwebtoken'),
+  config = require('@config');
---
+++ services/BudgetManagerAPI/app/setup/index.js
@@ -0,0 +1,8 @@
+const mongoose = require('mongoose'),
+  UserModel = require('@BudgetManagerModels/user');
+
+const models = {
+  User: mongoose.model('User')
+}
+
+module.export = models;
