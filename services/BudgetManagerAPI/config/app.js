5199a6f0579fc3961960638983a69b9f36b4623c
Darren <darrenhay1994@gmail.com>
2017-11-25 16:04:23 +0200

    setting up packages being used

---
+++ services/BudgetManagerAPI/config/app.js
@@ -0,0 +1,21 @@
+const express = require('express'),
+  app = express(),
+  bodyParser = require('body-parser'),
+  mongoose = require('mongoose'),
+  morgan = require('morgan'),
+  consign = require('consign'),
+  cors = require('cors'),
+  passport = require('passport'),
+  passportConfig = require('./passport')(passport),
+  jwt = require('jsonwebtoken'),
+  config = require('./index.js'),
+  database = require('./database')(mongoose, config);
+
+  app.use(express.static('.'));
+  app.use(bodyParser.urlencoded({ extended: true }));
+  app.use(bodyParser.json());
+  app.use(morgan('dev'));
+  app.use(cors());
+  app.use(passport.initialize());
+
+  app.set('budgetsecret'. config.secret);
