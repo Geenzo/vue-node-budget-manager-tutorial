d41cb457426db425c70d5e207448db36b9b804c0
Darren <darrenhay1994@gmail.com>
2017-11-28 13:43:12 +0200

    using consign to set up order of loading in setup

--- services/BudgetManagerAPI/config/app.js
+++ services/BudgetManagerAPI/config/app.js
@@ -11,11 +11,23 @@ const express = require('express'),
   config = require('./index.js'),
   database = require('./database')(mongoose, config);
 
-  app.use(express.static('.'));
-  app.use(bodyParser.urlencoded({ extended: true }));
-  app.use(bodyParser.json());
-  app.use(morgan('dev'));
-  app.use(cors());
-  app.use(passport.initialize());
+app.use(express.static('.'));
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+app.use(bodyParser.json());
+app.use(morgan('dev'));
+app.use(cors());
+app.use(passport.initialize());
 
-  app.set('budgetsecret'. config.secret);
+app.set('budgetsecret'.config.secret);
+
+consign({
+    cwd: 'services'
+  })
+  .include('BudgetManagerAPI/app/setup')
+  .then('BudgetManagerAPI/app/api')
+  .then('BudgetManagerAPI/app/routes')
+  .into(app);
+
+module.exports = app;
