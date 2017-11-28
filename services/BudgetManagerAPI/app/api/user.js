97fff7383cbccbfbebca52aa9a1414f272b343f8
Darren <darrenhay1994@gmail.com>
2017-11-28 14:46:51 +0200

    adding signup method

--- services/BudgetManagerAPI/app/api/user.js
+++ services/BudgetManagerAPI/app/api/user.js
@@ -18,3 +18,44 @@ api.setup = (User) => (req, res) => {
     });
   })
 }
+
+api.index = (User, BudgetToken) => (req, res) => {
+  const token = BudgetToken;
+
+  if (token) {
+    User.find({}, (error, users) => {
+      if (error) throw error;
+      res.status(200).json(users);
+    });
+  } else return res.status(403).send({
+    success: false,
+    message: 'Unathorized'
+  });
+}
+
+api.signup = (User) => (req, res) => {
+  if (!req.body.username || !req.body.password) res.json({
+    success: false,
+    message: 'Please, pass a username and password.'
+  });
+  else {
+    const newUser = new User({
+      username: req.body.username,
+      password: req.body.password,
+      clients: []
+    });
+
+    newUser.save((error) => {
+      if (error) return res.status(400).json({
+        success: false,
+        message: 'Username already exists.'
+      });
+      res.json({
+        success: true,
+        message: 'Account created successfully'
+      });
+    })
+  }
+}
+
+module.exports = api;
