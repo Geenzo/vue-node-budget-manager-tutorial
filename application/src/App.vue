1e7a07cdabfcc91bf52ec700ae3ade6103672d00
Darren <darrenhay1994@gmail.com>
2017-12-05 12:42:42 +0200

    adding styles

--- application/src/App.vue
+++ application/src/App.vue
@@ -13,5 +13,20 @@ export default {
 </script>
 
 <style lang="scss">
+  @import "./assets/styles";
 
+  body {
+    background: url('./assets/images/background.jpg') no-repeat center center fixed;
+    &:after {
+      content: '';
+      position: fixed;
+      width: 100%;
+      height: 100%;
+      top: 0;
+      left: 0;
+      background-color: $background-tint;
+      opacity: .3;
+      z-index: -1;
+    }
+  }
 </style>
