56618d54ee5794184f3bd189ceb453d6f7daa642
Darren <darrenhay1994@gmail.com>
2017-12-05 13:10:00 +0200

    creating authentication component

---
+++ application/src/components/pages/Authentication/index.js
@@ -0,0 +1,66 @@
+import Axios from 'axios';
+import router from '@/router';
+
+const BudgetManagerAPI = `http://${window.location.hostname}:3001`;
+
+export default {
+  user: {
+    authentication: false,
+  },
+
+  authenticate(context, credentials, redirect) {
+    Axios.post(`${BudgetManagerAPI}/api/v1/auth`, credentials)
+      .then(({
+        data: {
+          token,
+        },
+      }) => {
+        context.$cookie.set('token', token, '1D');
+        context.validLogin = true;
+        this.user.authenticated = true;
+
+        if (redirect) router.push(redirect);
+      }).catch(({
+        response: {
+          data,
+        },
+      }) => {
+        context.snackbar = true;
+        context.message = data.message;
+      });
+  },
+
+  signup(context, credentials, redirect) {
+    Axios.post(`${BudgetManagerAPI}/api/v1/signup`, credentials)
+      .then(({
+        data: {
+          token,
+        },
+      }) => {
+        context.$cookie.set('token', token, '1D');
+        context.validSignUp = true;
+        this.user.authenticated = true;
+
+        if (redirect) router.push(redirect);
+      }).catch(({
+        response: {
+          data,
+        },
+      }) => {
+        context.snackbar = true;
+        context.message = data.message;
+      });
+  },
+
+  checkAuthentication() {
+    const token = document.cookie;
+
+    if (token) this.user.authenticated = true;
+    else this.user.authenticated = false;
+  },
+
+  getAuthenticationHeader() {
+    return `Bearer ${context.$cookie.get('token')}`;
+  },
+
+}
