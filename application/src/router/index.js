ed15ba7d7d75441d069299a51893a414870eb1e4
Darren <darrenhay1994@gmail.com>
2017-12-06 21:57:39 +0200

    Updating router to add home component

--- application/src/router/index.js
+++ application/src/router/index.js
@@ -1,15 +1,40 @@
 import Vue from 'vue';
 import Router from 'vue-router';
+import * as Auth from '@/components/pages/Authentication';
 
 // Pages
+import Home from '@/components/pages/Home';
 import Authentication from '@/components/pages/Authentication/Authentication';
 
 Vue.use(Router);
 
-export default new Router({
+const router = new Router({
   routes: [{
-    path: '/login',
-    name: 'Authentication',
-    component: Authentication,
-  }],
+      path: '/',
+      name: 'Home',
+      component: Home,
+      meta: {
+        requiredAuth: true,
+      },
+    },
+    {
+      path: '/login',
+      name: 'Authentication',
+      component: Authentication,
+    }
+  ],
 });
+
+router.beforeEach((to, from, next) => {
+  if (to.meta.requiredAuth) {
+    if (Auth.default.user.authenticated) {
+      next();
+    } else {
+      router.push('/login');
+    }
+  } else {
+    next();
+  }
+});
+
+export default router;
