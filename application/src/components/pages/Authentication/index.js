baada1385a0335b7c0de8d945ca7c86ad84a2874
Darren <darrenhay1994@gmail.com>
2018-04-19 23:48:55 +0100

    updating signin method and adding signout method

--- application/src/components/pages/Authentication/index.js
+++ application/src/components/pages/Authentication/index.js
@@ -26,31 +26,27 @@ export default {
 
   signup(context, credentials, redirect) {
     Axios.post(`${BudgetManagerAPI}/api/v1/signup`, credentials)
-      .then(({
-        data: {
-          token,
-        },
-      }) => {
-        context.$cookie.set('token', token, '1D');
+      .then(() => {
         context.validSignUp = true;
-        this.user.authenticated = true;
 
-        if (redirect) router.push(redirect);
-      }).catch(({
-        response: {
-          data,
-        },
-      }) => {
+        this.authenticate(context, credentials, redirect);
+      }).catch(({ response: { data } }) => {
         context.snackbar = true;
         context.message = data.message;
       });
   },
 
+  signout(context, redirect) {
+    context.$cookie.delete('token');
+    context.$cookie.delete('user_id');
+    this.user.authenticated = false;
+
+    if (redirect) router.push(redirect);
+  },
+
   checkAuthentication() {
     const token = document.cookie;
-
-    if (token) this.user.authenticated = true;
-    else this.user.authenticated = false;
+    this.user.authenticated = !!token;
   },
 
   getAuthenticationHeader(context) {
