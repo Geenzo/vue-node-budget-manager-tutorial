216487682939afc5b7e098699e69852f364342fe
Darren <darrenhay1994@gmail.com>
2018-04-20 00:32:02 +0100

    creating budget list body component

---
+++ application/src/components/Budget/BudgetListBody.vue
@@ -0,0 +1,86 @@
+<template>
+  <section class="l-budget-body">
+      <div class="md-budget" v-if="budgets != null" v-for="budget in budgets">
+          <div class="md-budget-info white--text">{{ budget.client }}</div>
+          <div class="md-budget-info white--text">{{ budget.title }}</div>
+          <div class="md-budget-info white--text">{{ budget.state }}</div>
+          <div class="l-budget-actions">
+              <v-btn small flat color="light-blue lighten-1">
+                  <v-icon small>visibility</v-icon>
+              </v-btn>
+              <v-btn small flat color="yellow accent-1">
+                  <v-icon>mode_edit</v-icon>
+              </v-btn>
+              <v-btn small flat color="red lighten-1">
+                  <v-icon>delete_forever</v-icon>
+              </v-btn>
+          </div>
+      </div>
+  </section>
+</template>
+
+<script>
+    export default {
+        props: ['budgets']
+    }
+</script>
+
+<style lang="scss">
+    @import "./../../assets/styles";
+
+    .l-budget-body {
+        display: flex;
+        flex-direction: column;
+
+        .md-budget {
+            width: 100%;
+            display: flex;
+            flex-direction: column;
+            margin: 15px 0;
+
+            @media (min-width: 960px) {
+                flex-direction: row;
+                margin: 0;
+            }
+
+            .md-budget-info {
+                flex-basis: 25%;
+                width: 100%;
+                background-color: rgba(0, 175, 255, 0.45);
+                border: 1px solid $border-color-input;
+                padding: 0 15px;
+                display: flex;
+                height: 35px;
+                align-items: center;
+                justify-content: center;
+
+                &:first-of-type, &:nth-of-type(2) {
+                    text-transform: capitalize;
+                }
+
+                &:nth-of-type(3) {
+                    text-transform: uppercase;
+                }
+
+                @media (min-width: 601px) {
+                    justify-content: flex-start;
+                }
+            }
+        }
+
+        .l-budget-action {
+            flex-basis: 25%;
+            display: flex;
+            background-color: rgba(0, 175, 255, 0.45);
+            border: 1px solid $border-color-input;
+            align-items: center;
+            justify-content: center;
+
+            .btn {
+                min-width: 45px !important;
+                margin: 0 5px !important;
+            }
+        }
+    }
+</style>
+
