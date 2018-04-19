c557d14703c8b04cbbd9d90c140a45697a7ea12f
Darren <darrenhay1994@gmail.com>
2018-04-19 23:42:16 +0100

    updating authenticate method to also use user_id

--- application/src/components/pages/Authentication/index.js
+++ application/src/components/pages/Authentication/index.js
@@ -10,21 +10,15 @@ export default {
 
   authenticate(context, credentials, redirect) {
     Axios.post(`${BudgetManagerAPI}/api/v1/auth`, credentials)
-      .then(({
-        data: {
-          token,
-        },
-      }) => {
-        context.$cookie.set('token', token, '1D');
+      .then(({ data }) => {
+        context.$cookie.set('token', data.token, '1D');
+        context.$cookie.set('user_id', data.user._id, '1D');
         context.validLogin = true;
+
         this.user.authenticated = true;
 
         if (redirect) router.push(redirect);
-      }).catch(({
-        response: {
-          data,
-        },
-      }) => {
+      }).catch(({response: { data } }) => {
         context.snackbar = true;
         context.message = data.message;
       });
