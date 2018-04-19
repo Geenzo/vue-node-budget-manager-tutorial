0b491a481379a3418e7d1e54a862a5bd5f185547
Darren <darrenhay1994@gmail.com>
2018-04-20 00:50:52 +0100

    removing redundant endpoints

--- services/BudgetManagerAPI/app/api/user.js
+++ services/BudgetManagerAPI/app/api/user.js
@@ -2,37 +2,6 @@ const mongoose = require('mongoose');
 
 const api = {};
 
-api.setup = (User) => (req, res) => {
-  const admin = new User({
-    username: 'admin',
-    password: 'admin',
-    clients: []
-  });
-
-  admin.save(error => {
-    if (error) throw error;
-
-    console.log('Admin account was successfully set up');
-    res.json({
-      success: true
-    });
-  })
-}
-
-api.index = (User, BudgetToken) => (req, res) => {
-  const token = BudgetToken;
-
-  if (token) {
-    User.find({}, (error, users) => {
-      if (error) throw error;
-      res.status(200).json(users);
-    });
-  } else return res.status(403).send({
-    success: false,
-    message: 'Unathorized'
-  });
-}
-
 api.signup = (User) => (req, res) => {
   if (!req.body.username || !req.body.password) res.json({
     success: false,
@@ -42,7 +11,6 @@ api.signup = (User) => (req, res) => {
     const newUser = new User({
       username: req.body.username,
       password: req.body.password,
-      clients: []
     });
 
     newUser.save((error) => {
