a4de7e52143b6694ec78a615fe184d548436384a
Darren <darrenhay1994@gmail.com>
2018-04-23 23:09:48 +0100

    updating routers for budget and client

--- services/BudgetManagerAPI/app/routes/budget.js
+++ services/BudgetManagerAPI/app/routes/budget.js
@@ -9,4 +9,12 @@ module.exports = (app) => {
         .post(passport.authenticate('jwt', config.session), api.store(models.User, models.Budget, models.Client, app.get('budgetsecret')))
         .get(passport.authenticate('jwt', config.session), api.getAll(models.User, models.Budget, app.get('budgetsecret')))
         .get(passport.authenticate('jwt', config.session), api.getAllFromClient(models.User, models.Budget, app.get('budgetsecret')))
+        .delete(passport.authenticate('jwt', config.session), api.remove(models.User, models.Budget, models.Client, app.get('budgetsecret')))
+
+    app.route('/api/v1/budget/single')
+        .get(passport.authenticate('jwt', config.session), api.index(models.User, models.Budget, models.Client, app.get('budgetsecret')))
+        .put(passport.authenticate('jwt', config.session), api.edit(models.User, models.Budget, models.Client, app.get('budgetsecret')))
+
+    app.route('/api/v1/budget/state')
+        .get(passport.authenticate('jwt', config.session), api.getByState(models.User, models.Budget, models.Client, app.get('budgetsecret')))
 }
--- services/BudgetManagerAPI/app/routes/client.js
+++ services/BudgetManagerAPI/app/routes/client.js
@@ -7,5 +7,10 @@ module.exports = (app) => {
 
     app.route('/app/v1/client')
         .post(passport.authenticate('jwt', config.session), api.store(models.User, models.Client, app.get('budgetsecret')))
-        .get(passport.authenticate('jwt', config.session), api.getAll(models.User, models.Client, app.get('budgetsecret')));
+        .get(passport.authenticate('jwt', config.session), api.getAll(models.User, models.Client, app.get('budgetsecret')))
+        .delete(passport.authenticate('jwt', config.session), api.remove(models.User, models.Client, app.get('budgetsecret')))
+
+    app.route('/api/v1/client/single')
+        .get(passport.authenticate('jwt', config.session), api.index(models.User, models.Client, app.get('budgetsecret')))
+        .put(passport.authenticate('jwt', config.session), api.edit(models.User, models.Client, app.get('budgetsecret')))
 }
