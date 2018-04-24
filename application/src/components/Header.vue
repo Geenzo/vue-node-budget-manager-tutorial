e40a026b5290bed54169a335bb633b6d406d8967
Darren <darrenhay1994@gmail.com>
2018-04-24 21:05:35 +0100

    updating header component

--- application/src/components/Header.vue
+++ application/src/components/Header.vue
@@ -2,10 +2,10 @@
     <header class="1-header-container">
         <v-layout row wrap :class="budgetsVisible ? 'l-budgets-header' : 'l-clients-header'">
             <v-flex xs12 md5>
-                <v-text-field v-model="search"
+                <v-text-field v-model="searchValue"
                                 label="Search"
                                 append-icon="search"
-                                color="budgetsVisible ? 'light-blue lighten-1' : 'green lighten-1'">
+                                :color="budgetsVisible ? 'light-blue lighten-1' : 'green lighten-1'">
                 </v-text-field>
             </v-flex>
 
@@ -22,7 +22,8 @@
                             :color="budgetsVisible ? 'light-blue lighten-1' : 'green lighten-1'"
                             v-model="status"
                             :items="statusItems"
-                            single-line>
+                            single-line
+                            @change="selectState">
                 </v-select>
             </v-flex>
 
@@ -36,15 +37,23 @@
 <script>
     import Authentication from '@/components/pages/Authentication';
     export default {
-        props: ['budgetsVisible'],
+        props: ['budgetsVisible', 'selectState', 'search'],
         data() {
             return {
                 search: '',
                 status: '',
                 statusItems: [
-                    'All', 'Approved', 'Denied', 'Waiting', 'Writing', 'Editing'
+                    'all', 'approved', 'denied', 'waiting', 'writing', 'editing'
                 ]
             }
+        },
+        watch: {
+            'searchValue': function () {
+                this.$emite('input', this.searchValue)
+            }
+        },
+        created () {
+            this.searchValue = this.search
         },
         methods: {
             submitSignout() {
@@ -55,35 +64,39 @@
 </script>
 
 <style lang="scss">
-    @import "./../assets/styles";
+  @import "./../assets/styles";
 
-    .l-header-container {
-        background-color: $background-color;
-        margin: 0 auto;
-        padding: 0 15px;
-        min-width: 272px;
+  .l-header-container {
+    background-color: $background-color;
+    margin: 0 auto;
+    padding: 0 15px;
+    min-width: 272px;
 
-        .l-budgets-header {
-            label, input, .icon, .input-group__selections__comma {
-                color: #29b6f6!important;
-            }
-        }
-
-        .l-clients-header {
-            label, input, .icon, .input-group__selections__comma {
-                color: #66bb6a!important;
-            }
-        }
-
-        .input-group__details {
-            &:before {
-                background-color: $border-color-input !important;
-            }
-        }
-
-        .btn {
-            margin-top: 15px;
-        }
+    .l-budgets-header {
+      label, input, .icon, .input-group__selections__comma {
+        color: #29b6f6!important;
+      }
     }
+
+    .l-clients-header {
+      label, input, .icon, .input-group__selections__comma {
+        color: #66bb6a!important;
+      }
+    }
+
+    .input-group__details {
+      &:before {
+        background-color: $border-color-input !important;
+      }
+    }
+
+    .btn {
+      margin-top: 15px;
+    }
+  }
+  
+  .list__tile__title, .input-group__selections {
+    text-transform: uppercase !important;
+  }
 </style>
 
